459222e8e87e5add8f5fbb86bce796736516d491
Théo Megy <theo.megy@logiroad.fr>
2022-08-03 17:43:55 +0200

    install and run on cracks dataset bis

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from . import experiment_utils
---
+++ data/cracks_classnames.py
@@ -0,0 +1,8 @@
+cracks_name_map = {
+    0 : 'Arrachement_pelade',
+    1 : 'Faiencage',
+    2 : 'Nid_de_poule',
+    3 : 'Transversale',
+    4 : 'Longitudinale',
+    5 : 'Reparation',
+    }
